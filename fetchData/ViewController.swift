df00c5234008db31bb461c6e0a6a16811480219e
KorobskoyRoman <shopone023@gmail.com>
2021-11-25 18:53:25 +0300

    Initial Commit

---
+++ fetchData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fetchData
+//
+//  Created by Roman Korobskoy on 25.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
